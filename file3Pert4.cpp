8b8ea16da2d2a27f762f80b79295184ad31dd983
alvinwahyupratama2412 <alvinwahyupratama22@gmail.com>
2025-03-18 12:06:03 +0700

    membuat deklarassi variable dan membuat menu

--- file3Pert4.cpp
+++ file3Pert4.cpp
@@ -7,3 +7,17 @@ float hitungLuasPersegiPanjang(float p, float l){
 float hitungLuasSegitiga(float a, float t){
     return 0.5 * a * t;
 }
+
+int main(){
+    int pilihan;
+    float panjang,lebar,alas,tinggi;
+    do{
+        cout << "===================MENU===================" << endl;
+        cout << "1. Menghitung luas persegi panjang" << endl;
+        cout << "2. Menghitung luas segitiga" << endl;
+        cout << "3. Exit" << endl;
+        cout << "Pilihan(1/2/3) :";
+        cin >> pilihan;
+        
+    }
+}
