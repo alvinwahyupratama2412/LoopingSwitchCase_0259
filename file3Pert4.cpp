a09f9107a6f048aa230b35395a22ccceb2c14068
alvinwahyupratama2412 <alvinwahyupratama22@gmail.com>
2025-03-18 11:58:50 +0700

    membuat pustaka dan fungsi untuk menghitung luas persegipanjang dan segitiga

---
+++ file3Pert4.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+float hitungLuasPersegiPanjang(float p, float l){
+    return p*l;
+}
+float hitungLuasSegitiga(float a, float t){
+    return 0.5 * a * t;
+}
